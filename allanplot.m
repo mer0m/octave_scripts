d5d0b7e46998acbc435d46715a2ebd7f6a739ef6
bma <baptiste.marechal@femto-st.fr>
2017-10-17 10:32:23 +0200

    dyn tau array size

--- allanplot.m
+++ allanplot.m
@@ -7,6 +7,6 @@ mult = eval(argv(){3});
 data.freq = load(filename)(:,col).*mult;
 data.rate = 1;
 
-ad = allan(data, 2.^(0:12), strsplit(filename, '/'){end}, 1);
+ad = allan(data, 2.^(0:nextpow2(length(data.freq))), strsplit(filename, '/'){end}, 1);
 input("Press to continue...");
 exit
