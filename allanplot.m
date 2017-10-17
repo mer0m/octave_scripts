2f13b73569577e6baf7085cc4b9b9ac366408254
bma <baptiste.marechal@femto-st.fr>
2017-10-17 21:59:33 +0200

    add multiple column option

--- allanplot.m
+++ allanplot.m
@@ -4,9 +4,20 @@ filename = argv(){1};
 col = eval(argv(){2});
 mult = eval(argv(){3});
 
-data.freq = load(filename)(:,col).*mult;
-data.rate = 1;
-
-ad = allan(data, 2.^(0:nextpow2(length(data.freq))), strsplit(filename, '/'){end}, 1);
-input("Press to continue...");
+if length(col) == length(mult)
+    figure
+    hold all
+    grid on
+    cc = 'bkcgmry';
+    for i = [1:length(col)]
+        data.freq = load(filename)(:,col(i)).*mult(i);
+        data.rate = 1;
+        [ad, S, err, tau] = allan(data, 2.^[0:nextpow2(length(data.freq))-3]./data.rate, strcat(strsplit(filename, '/'){end}, num2str(i)), 0);
+        loglogerr(tau, ad, err, cc(mod(i, length(cc))))
+        leg{i} = strcat(filename, ' col', num2str(col(i)));
+        hold on
+    end
+    legend(leg)
+    input("Press to continue...");
+end
 exit
