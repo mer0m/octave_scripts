c4b4eae8c4b21936212c6fb0b0ea48babdd1dc6d
bma <baptiste.marechal@femto-st.fr>
2018-07-13 11:35:29 +0200

    psdplot.m: add n-diff arg

--- psdplot.m
+++ psdplot.m
@@ -4,6 +4,7 @@ filename = argv(){1};
 col = eval(argv(){2});
 mult = eval(argv(){3});
 data.rate = eval(argv(){4});
+d = eval(argv(){5});
 
 if length(col) == length(mult)
 	figure
@@ -11,7 +12,7 @@ if length(col) == length(mult)
 	grid on
 	cc = 'bkcgmry';
 	for i = [1:length(col)]
-		data.freq = load(filename)(:,col(i)).*mult(i);
+		data.freq = diff(load(filename)(:,col(i)).*mult(i), d)*data.rate**d;
 		[p, f] = pwelch(data.freq, hanning(length(data.freq)/1), 0.5, [], data.rate, 'onesided', 'power');
 		semilogx(f, 10*log10(p), cc(mod(i, length(cc))))
 		leg{i} = strcat(filename, ' col', num2str(col(i)));
