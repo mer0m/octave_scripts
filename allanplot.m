146b73337a99e14c93be6fd01e60ce532362c97c
bma <baptiste.marechal@femto-st.fr>
2018-11-29 10:17:58 +0100

    save long without truncature

--- allanplot.m
+++ allanplot.m
@@ -52,7 +52,7 @@ if length(col) == length(mult)
 		if arg_save ==1
 			filenameout = strcat(strsplit(filename,'.'){1},'-',num2str(col(i)),'.sig')
 			datatosave = horzcat(tau', ad', err');
-			save('-text', filenameout , 'datatosave');
+			save('-ascii', '-double', '-tabs', filenameout , 'datatosave');
 		end
 	end
 	legend(leg)
--- temp2allan.m
+++ temp2allan.m
@@ -30,5 +30,5 @@ tosave = tau';
 for i = [1:length(col)]
 	tosave = horzcat(tosave, ad_tosave{i});
 end
-save('-text', strcat(strsplit(filename, '.'){1}, '_ad.dat'), 'tosave');
+save('-ascii', '-double', '-tabs', strcat(strsplit(filename, '.'){1}, '_ad.dat'), 'tosave');
 end
